4b81ebcc803586e4a079c9c80d49361f7ac8b7fe
tjvananne <tjvananne@gmail.com>
2024-05-26 17:11:46 -0600

    Scanner implementation with comments and notes

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # crafting_interpreters_notes
 My personal notes and code while following along in the Crafting Interpreters book.
+
+Implementation notes (all from project root dir):
+
+* Build out the `com/craftinginterpreters/lox/Lox.java` path
+* Compile with `javac com/craftinginterpreters/lox/Lox.java`
+* Execute with `java com.craftinginterpreters.lox.Lox [source-code-file]`
+    * `[source-code-file]` is optional. Omitting it will enter the REPL in the terminal.
+
+
+## Scanning
+
+The scanner (aka "lexer") is responsible for reading each character of the raw text source code file (or REPL input) and converting that into Tokens to be used by the Parser in the next phase. Much of the scanner is simply a glorified switch statement with some extra logic. 
+
+Part of that extra logic there disambiguates between the different types of tokens (e.g. recognizing a `<=` operator instead of a `<` and `=` separately). This is an example of the **maximal munch** principle.
+
+> When two lexical grammar rules can both match a chunk of code that the scanner is looking at, *whichever one matches the most characters wins.*
+
+Most of the time, we "consume" a character when reading the source code (i.e. through the `advance()` method). In order to satisfy the maximal munch principle, we sometimes need to peek ahead of the next character to determine the operator, identifier, or literal that is being referred to in the code. This is what the `peek()` and `peekNext()` methods are for.
+
+> Maximal munch means we can't easily detect a reserved word until we've reached the end of what might instead be an identifier.
+
+### Reflections
+
+It makes much more sense now why an identifier such as a variable can't start with a number of symbol (other than `_`) in most languages. It would add a lot more complexity to identifying numeric literals. If I add `42x` to my source code, should this be flagged as a Lexical Error because the numeric literal contains an alpha character? Or should this be interpreted as an identifier by that name?
+
---
+++ com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,226 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+// Note: static imports are not best practice!
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Scanner {
+    private final String source;
+
+    // The "<>()" is "the diamond"
+    // https://docs.oracle.com/javase/tutorial/java/generics/types.html#diamond
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // We are at the beginning of the next lexeme.
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    // This is the real "heart" of the scanner:
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            // single-char tokens
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            // multi-char tokens; I'm guessing match will be a look-ahead
+            // that doesn't "consume" the character in the same manner as advance()?
+            // Oh, no need to look ahead because "current" is already the index of
+            // the next character in the source.
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            // Ah, ok so peek() is what I was thinking match would be. peek()
+            // is a non-consuming look-ahead.
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace
+                break;
+            
+            case '\n':
+                line++;
+                break;
+            
+            case '"': string(); break;
+
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+                break;
+        }
+    }
+
+    private void identifier() {
+        // Load the whole identifier. Determine if it's a reserved
+        // keyword in the language. Return either the type of that
+        // reserved keyword or the IDENTIFIER type.
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "." and continue consuming numbers
+            advance();
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+            Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        // If Lox supported escapce sequences like \n, then we'd
+        // unescape those here.
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    private boolean match(char expected) {
+        // match is like a conditional advance()
+        // only advance if the character is what we expected
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private char peek() {
+        // Inspect the next character to be consumed without actually
+        // consuming it.
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private char peekNext() {
+        // Inspect 1 character after the next character to consume
+        // without actually consuming it. Useful for parsing numeric
+        // literals.
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+}
